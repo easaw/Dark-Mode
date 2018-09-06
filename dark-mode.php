a21fa6dcbe5305360665494f3661279c27ce704e
Gary Jones <gary@gamajo.com>
2018-09-06 08:25:38 +0100

    CS: Fix PHPCS violations
    Fixes #122.

--- dark-mode.php
+++ dark-mode.php
@@ -1,5 +1,13 @@
 <?php
 /**
+ * Dark Mode main plugin file
+ *
+ * @package      danieltj27/Dark-Mode
+ * @author       Daniel James
+ * @copyright    2018 Daniel James
+ * @license      GPL-2.0-or-later
+ *
+ * @wordpress-plugin
  * Plugin Name: Dark Mode
  * Plugin URI: https://wordpress.org/plugins/dark-mode/
  * Description: Lets your users make the WordPress admin dashboard darker.
@@ -13,6 +21,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
-require( 'class-dark-mode.php' );
+require __DIR__ . '/class-dark-mode.php';
 
 $dark_mode = new Dark_Mode();
